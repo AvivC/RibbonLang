27e6d86b0e9f627c16138733458c3e6d5414f809
Aviv Cohn <avivcohn1@gmail.com>
2019-10-16 23:14:59 +0300

    Added tests for the module system

--- python/tester/runtests.py
+++ python/tester/runtests.py
@@ -12,16 +12,23 @@ INPUT_PATH = os.path.join('..', '..', 'code.pln')
 INTERPRETER_ARGS = ''
 
 
-def _run_on_interpreter(interpreter_path, input_text):
+def _run_on_interpreter(interpreter_path, input_text, additional_files):
     input_file_name = f'{str(uuid.uuid4())}.pln'
     with open(input_file_name, 'w') as f:
         f.write(input_text)
 
     try:
+        for file_name, file_code in additional_files.items():
+            file_path = _relative_path_to_abs(os.path.join('..', '..', file_name))
+            with open(file_path, 'w') as f:
+                f.write(file_code)
+
         interpreter_cmd = f'{interpreter_path} {input_file_name} {INTERPRETER_ARGS}'
         output = subprocess.run(interpreter_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     finally:
         os.remove(input_file_name)
+        for additional_file_name in additional_files:
+            os.remove(additional_file_name)
 
     output_text = output.stdout.decode().replace('\r\n', '\n')
 
@@ -35,10 +42,10 @@ def _run_on_interpreter(interpreter_path, input_text):
     return output_text[:memory_diagnostics_start]
 
 
-def _parse_test_lines(lines_iter, end_token):
-    expect_lines = []
+def _parse_test_lines(lines_iter, end_tokens):
+    lines = []
     line = next(lines_iter)
-    while line.strip() != end_token:
+    while line.strip() not in end_tokens:
         if line == '\n':  # allow a special case of bare empty lines, for convenience
             indent = 0
         elif line.startswith('\t'):
@@ -51,10 +58,10 @@ def _parse_test_lines(lines_iter, end_token):
             return None
 
         line = line[indent:]  # deindent
-        expect_lines.append(line)
+        lines.append(line)
         line = next(lines_iter)
 
-    return ''.join(expect_lines)
+    return ''.join(lines)
 
 
 def _run_test_file(absolute_path):
@@ -62,6 +69,7 @@ def _run_test_file(absolute_path):
         text = f.readlines()
 
     all_success = True
+    num_tests = 0
 
     lines = iter(text)
     while True:
@@ -91,18 +99,69 @@ def _run_test_file(absolute_path):
 
         print('Test %-50s' % test_name, end='')
 
-        test_code = _parse_test_lines(lines, 'expect')
+        num_tests += 1
+
+        test_lines = []
+        line = next(lines)
+        while line.strip() != 'expect' and not line.strip().startswith('file'):
+            if line == '\n':  # allow a special case of bare empty lines, for convenience
+                indent = 0
+            elif line.startswith('\t'):
+                indent = 1
+            elif line.startswith('    '):
+                indent = 4
+            else:
+                print()
+                print('Parsing error: Indentation missing in test code or expected result.')
+                test_code = None
+                break
+
+            line = line[indent:]  # deindent
+            test_lines.append(line)
+            line = next(lines)
+
+        else:
+            test_code = ''.join(test_lines)
+
         if test_code is None:
             all_success = False
             break
 
-        expect_output = _parse_test_lines(lines, 'end')
+        additional_files = {}
+        while line.strip().startswith('file '):
+            file_name = line.split(' ')[1].strip() + '.pln'
+
+            additional_file_lines = []
+            line = next(lines)
+            while line.strip() != 'expect' and not line.strip().startswith('file'):
+                if line == '\n':  # allow a special case of bare empty lines, for convenience
+                    indent = 0
+                elif line.startswith('\t'):
+                    indent = 1
+                elif line.startswith('    '):
+                    indent = 4
+                else:
+                    print()
+                    print('Parsing error: Indentation missing in test code or expected result.')
+                    additional_file_code = None
+                    break
+
+                line = line[indent:]  # deindent
+                additional_file_lines.append(line)
+                line = next(lines)
+
+            else:
+                additional_file_code = ''.join(additional_file_lines)
+
+            additional_files[file_name] = additional_file_code
+
+        expect_output = _parse_test_lines(lines, ['end'])
         if expect_output is None:
             all_success = False
             break
 
         interpreter_path = _relative_path_to_abs(os.path.join('..', '..', INTERPRETER_NAME))
-        output = _run_on_interpreter(interpreter_path, test_code)
+        output = _run_on_interpreter(interpreter_path, test_code, additional_files)
         if output == expect_output:
             print(f'SUCCESS')
         else:
@@ -122,7 +181,7 @@ def _run_test_file(absolute_path):
                 else:
                     print()
 
-    return all_success
+    return all_success, num_tests
 
 
 def _relative_path_to_abs(path):
@@ -131,6 +190,7 @@ def _relative_path_to_abs(path):
 
 
 def _run_test_directory(relative_dir_path):
+    num_tests = 0
     all_success = True
     abs_dir_path = _relative_path_to_abs(relative_dir_path)
     for f in os.listdir(abs_dir_path):
@@ -138,10 +198,12 @@ def _run_test_directory(relative_dir_path):
         if os.path.isfile(abs_test_path) and abs_test_path.endswith(TEST_FILE_SUFFIX):
             print(f'Running tests in {f}')
             print()
-            if not _run_test_file(abs_test_path):
+            file_all_success, num_tests_in_file = _run_test_file(abs_test_path)
+            num_tests += num_tests_in_file
+            if not file_all_success:
                 all_success = False
             print()
-    return all_success
+    return all_success, num_tests
 
 
 def main():
@@ -150,7 +212,9 @@ def main():
     else:
         testdir = DEFAULT_TESTS_DIR
 
-    all_success = _run_test_directory(testdir)
+    all_success, num_tests = _run_test_directory(testdir)
+
+    print(f'Found total {num_tests} tests.')
 
     if all_success:
         print('All tests successful')
